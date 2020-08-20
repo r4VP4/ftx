165a344fd75d93aef97d2a7f4577b50690cd0ce9
T 570 2 <T5702@wp1074399.server-he.de>
2020-08-20 14:58:23 +0200

    0.0.011 added parameter markets to get_fills

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ftx",
-    version="0.0.010",
+    version="0.0.011",
     author="VvWkQkTKJ",
     author_email="VvWkQkTKJ@protonmail.ch",
     description="ftx client forked from https://github.com/ftexchange/ftx",
